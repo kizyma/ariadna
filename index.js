7ced004c4939739d2fcd91161bdf147461678c45
strangerin <kizimayarik01@gmail.com>
2024-01-25 15:00:09 +0200

    added vectorize and save mechanism to weaviate

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // index.js
 import weaviate from 'weaviate-ts-client';
-import { schemaCfg } from './config.js'; // Use named import
+import { readFileSync, readdirSync } from 'fs';
+import { join } from 'path'
+import { schemaCfg } from './config.js';
+
 
 const client = weaviate.client({
     scheme: 'http',
@@ -9,11 +12,38 @@ const client = weaviate.client({
 
 // step 1
 const schemaQry = await client.schema.getter().do();
-console.log(schemaQry);
+// console.log(schemaQry);
 
 // step 2 - something like a create/migrate command in an ordinary database
 // perform once to create, or once schema is changed
-await client.schema
-    .classCreator()
-    .withClass(schemaCfg)
-    .do();
+// await client.schema
+//     .classCreator()
+//     .withClass(schemaCfg)
+//     .do();
+
+// step 3 - upload images
+const imgFolder = './img';
+
+const files = readdirSync(imgFolder);
+
+for (const file of files) {
+    const filePath = join(imgFolder, file);
+
+    // Read the file content
+    const img = readFileSync(filePath);
+
+    // Convert the image content to base64
+    const b64 = Buffer.from(img).toString('base64');
+
+    // Use the filename (excluding extension) as text
+    const fileNameWithoutExtension = file.split('.')[0];
+
+    // Create Image instance in collection for each image
+    await client.data.creator()
+        .withClassName('Image')
+        .withProperties({
+            image: b64,
+            text: fileNameWithoutExtension,
+        })
+        .do();
+}
