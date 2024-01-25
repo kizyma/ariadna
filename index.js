2ddc657f355354c9290d86de0fbc6977c7674dda
strangerin <kizimayarik01@gmail.com>
2024-01-25 16:54:46 +0200

    - added basic search functionality - added some funny images to populate DB and search from

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // index.js
 import weaviate from 'weaviate-ts-client';
-import { readFileSync, readdirSync } from 'fs';
+import { readFileSync, readdirSync, writeFileSync } from 'fs';
 import { join } from 'path'
 import { schemaCfg } from './config.js';
 
+const imgFolder = './img';
+
+function toBase64(filePath) {
+    const img = readFileSync(filePath);
+    return Buffer.from(img).toString('base64');
+}
 
 const client = weaviate.client({
     scheme: 'http',
@@ -22,28 +28,33 @@ const schemaQry = await client.schema.getter().do();
 //     .do();
 
 // step 3 - upload images
-const imgFolder = './img';
+// const files = readdirSync(imgFolder);
+//
+// const batchProcess = files.map(async (imgFile) => {
+//     // Convert the image content to base64
+//     const b64 = toBase64(join(imgFolder, imgFile)); // Fix the path
+//     // Create Image instance in collection for each image
+//     await client.data.creator()
+//         .withClassName('Image')
+//         .withProperties({
+//             image: b64,
+//             text:  imgFile.split('.')[0],
+//         })
+//         .do();
+// });
+//
+// await Promise.all(batchProcess);
 
-const files = readdirSync(imgFolder);
+//step 4 search images
+const test = Buffer.from( readFileSync('./deathwatch_meme_test.jpeg') ).toString('base64');
 
-for (const file of files) {
-    const filePath = join(imgFolder, file);
+const resImage = await client.graphql.get()
+    .withClassName('Image')
+    .withFields(['image'])
+    .withNearImage({ image: test })
+    .withLimit(1)
+    .do();
 
-    // Read the file content
-    const img = readFileSync(filePath);
-
-    // Convert the image content to base64
-    const b64 = Buffer.from(img).toString('base64');
-
-    // Use the filename (excluding extension) as text
-    const fileNameWithoutExtension = file.split('.')[0];
-
-    // Create Image instance in collection for each image
-    await client.data.creator()
-        .withClassName('Image')
-        .withProperties({
-            image: b64,
-            text: fileNameWithoutExtension,
-        })
-        .do();
-}
+// Write result to filesystem
+const result = resImage.data.Get.Image[0].image;
+writeFileSync('./result.jpg', result, 'base64');
